18056338515866b69ba90232ec65c92d0f06af69
Sachin Kumar <sachinkumarv007@gmail.com>
2024-12-05 23:13:46 +0530

    container config done

---
+++ 05_file/four.js
@@ -0,0 +1,14 @@
+const coding =["js","java","python"]
+ const values=coding.forEach( (item)=>{
+    //    console.log(item);
+    return item
+})
+//console.log(values);
+
+
+const myNums=[1,2,3,4,5,6]
+ const newNums=myNums.filter( (num)=>num>3)
+ console.log(newNums);
+
+
+ 
