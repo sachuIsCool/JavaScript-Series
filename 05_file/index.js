aa1e26347d86a8ffb487aedf0b7a2b488a1992ec
Sachin Kumar <sachinkumarv007@gmail.com>
2024-12-05 22:04:28 +0530

    ..

--- 05_file/index.js
+++ 05_file/index.js
@@ -1,1 +1,32 @@
-console.log("Hello Wolrd");
+//for of loop
+
+
+const arr =[1,2,3,4,5]
+    for (const num of arr)
+    {
+console.log(num);
+    }
+
+
+   const greeting= "hello world"
+   for(const greet of greeting){
+    console.log(`Each char is ${greet}`)
+   } 
+
+  //maps in javascript 
+
+  const map =new Map()
+  map.set('IN',"INDIA")
+  map.set('FR',"FRANCE")
+  map.set('Sh',"FRANCE")
+  //console.log(map);
+
+  for(const [key,value] of map)
+  {
+console.log(key,':-',value);
+  }
+
+
+
+
+   
